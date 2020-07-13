aa7a7e6afc1699c7c26a21f995881f5d6498bb31
German Quinteros <german.quinteros.1993@gmail.com>
2020-07-13 01:58:58 -0300

    deploy in Heroku

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+const publicPath = path.resolve(__dirname, '../dist/angular-css-modules');
+app.use(express.static(publicPath));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Escuchando puerto ${process.env.PORT}`);
+});
