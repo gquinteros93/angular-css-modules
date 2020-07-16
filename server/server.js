a614da03d7bab5c73e9dcc163eb596d1464fa066
German Quinteros <german.quinteros.1993@gmail.com>
2020-07-15 22:27:47 -0300

    fixing routing

--- server/server.js
+++ server/server.js
@@ -12,7 +12,11 @@ app.use(bodyParser.json())
 
 const publicPath = path.resolve(__dirname, '../dist/angular-css-modules');
 app.use(express.static(publicPath));
-
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'../dist/angular-css-module/index.html'));
+  }
+);
+  
 app.listen(process.env.PORT, () => {
   console.log(`Escuchando puerto ${process.env.PORT}`);
 });
