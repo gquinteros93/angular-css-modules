d6db29c8ef67ae666509bb6e630c01af296074ce
paul-buechner <paul-buechner@outlook.de>
2021-01-26 21:45:31 +0100

    formatting/restructuring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/info',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "/info",
+    pathMatch: "full",
   },
   {
-    path: 'info',
-    loadChildren: () => import('./info/info.module').then(mod => mod.InfoModule)
+    path: "info",
+    loadChildren: () =>
+      import("./info/info.module").then((mod) => mod.InfoModule),
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
